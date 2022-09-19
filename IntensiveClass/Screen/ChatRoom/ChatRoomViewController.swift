eef1fc73f595610b2aa4bbd6860a1082dc8e69e8
Salsabila Azaria <salsabila.azaria@dana.id>
2022-09-19 13:20:28 +0700

    create base table view

---
+++ IntensiveClass/Screen/ChatRoom/ChatRoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatRoomViewController.swift
+//  IntensiveClass
+//
+//  Created by Salsabila Azaria on 19/09/22.
+//
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
