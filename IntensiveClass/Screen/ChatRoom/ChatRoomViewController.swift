90eb50a1218a00b760f401c2dd6ac6325988e6b7
Salsabila Azaria <salsabila.azaria@dana.id>
2022-09-19 16:07:20 +0700

    bikin uinnya dynamic kalau keyboardnya show /hide

--- IntensiveClass/Screen/ChatRoom/ChatRoomViewController.swift
+++ IntensiveClass/Screen/ChatRoom/ChatRoomViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class ChatRoomViewController: UIViewController {
 
+	@IBOutlet weak var stackViewBottomConst: NSLayoutConstraint!
+	
 	@IBOutlet weak var tableView: UITableView!
+	
 	override func viewDidLoad() {
         super.viewDidLoad()
 		setupTableView()
@@ -17,27 +20,52 @@ class ChatRoomViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 	
-	private func setupTableView() {
-//		self.tableView.register(UINib.init(nibName: "tableview", bundle: nil), forCellReuseIdentifier: "r")
-//		tableView.delegate = self
-//		tableView.dataSource = self
-		tableView.tableFooterView = UIView()
-//
-//		tableView.estimatedRowHeight = 600
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		NotificationCenter.default.addObserver(self, selector: #selector(showKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+	}
+	
+	@objc private func showKeyboard(_ notification: Notification) {
+		guard let userInfo = notification.userInfo,
+			  let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+			return
+		}
+		let safeAreaInset = UIApplication.shared.keyWindow?.safeAreaInsets ?? UIEdgeInsets.zero
+		self.stackViewBottomConst.constant = safeAreaInset.bottom - keyboardFrame.height
+	}
+	
+	@objc private func hideKeyboard(_ notification: Notification) {
+		self.stackViewBottomConst.constant = 0
 	}
 	
 	
-	
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	private func setupTableView() {
+		tableView.delegate = self
+		tableView.dataSource = self
+		tableView.tableFooterView = UIView()
+	}
 
 }
+
+extension ChatRoomViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+}
+
+extension ChatRoomViewController: UITableViewDelegate {
+	
+}
+
+
